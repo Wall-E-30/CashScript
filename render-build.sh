1a3729b1ee667b6cbc8c99a3221b66e801104953
Wall-E-30 <tilakjaipalgaur31@gmail.com>
2026-09-07 10:03:16 +0530

    Remove render-build.sh and switch to Docker

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Install Tesseract OCR engine and English language pack
-apt-get update && apt-get install -y tesseract-ocr tesseract-ocr-eng
-
-# Install Python dependencies
-pip install -r requirements.txt
